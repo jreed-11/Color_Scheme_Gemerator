14a8e9d68b0e91564473403911eaf210047a8389
jreed-11 <98095541+jreed-11@users.noreply.github.com>
2023-06-30 14:35:38 +0100

    JS file updated

---
+++ index.js
@@ -0,0 +1,46 @@
+let colors = []
+let colorSelection = document.getElementById("colorSelection")
+let colorScheme = document.getElementById("colorScheme")
+const colorContainer = document.getElementById("color-container")
+const hexValueContainer = document.getElementById("hex-container")
+
+document.getElementById("submit_btn").addEventListener('click', function(){
+    getColorScheme()
+   
+})
+
+
+
+
+function getColorScheme() {
+    let hexValue = colorSelection.value.slice(1)
+    let colorMode = colorScheme.value
+
+    fetch(`https://www.thecolorapi.com/scheme?hex=${hexValue}&${colorMode}&count=5`, {
+    method:"GET"
+})
+
+    .then((resp) => resp.json())
+    .then((data) => {
+        colors = data.colors.map((color) => {
+            return color.hex.value
+        })
+
+        let colorHtml = ``
+        let hexValueHtml = ``
+        for (let i =0; i < colors.length; i++) {
+            colorHtml += `
+                <div class="color" id=${colors[i]} data-color-hex-value="hexColor" 
+                style="background-color: ${colors[i]};"></div>
+            `
+            hexValueHtml += `
+            <div class="color" id=${colors[i]} data-color-hex-value="hexColor">
+            ${colors[i]}</div>
+            `
+        }
+
+        colorContainer.innerHTML = colorHtml
+        hexValueContainer.innerHTML = hexValueHtml        
+    })
+}
+
